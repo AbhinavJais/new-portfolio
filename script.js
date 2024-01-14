0d4712390fd0d5a55fe40923d53b1c96a0d5746d
Abhinav Jaiswal <114914635+AbhinavJais@users.noreply.github.com>
2024-01-14 20:47:08 +0530

    Updates CSS and JS

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var tl = gsap.timeline();
+
+tl.from(".navbar h4, .leftsidenav",{
+    y: 20,
+    delay:.7,
+    opacity: 0,
+    stagger:0.1,
+})
+tl.from("#center",{
+    y: 20,
+    opacity: 0,
+    stagger:0.2,
+})
+
+
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
