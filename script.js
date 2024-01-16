3eb9dd9243889d8e34b8ab8d0d336a9ff9d3aeaa
Abhinav Jaiswal <114914635+AbhinavJais@users.noreply.github.com>
2024-01-16 22:45:38 +0530

    updated code

--- script.js
+++ script.js
@@ -12,6 +12,35 @@ tl.from("#center",{
     stagger:0.2,
 })
 
+gsap.from("#projectpage h1, .project-content",{
+    y:100,
+    opacity:0,
+    delay: 1,
+    scrollTrigger:{
+        trigger:"#projectpage",
+        trigger:".project-content",
+        scroller:"body",
+        // markers:true,
+        start:"50",
+        end:"600",
+        scrub:4,
+    }
+})
+
+gsap.from("#skillspage h1, .skills-content",{
+    y:100,
+    opacity:0,
+    delay: 1,
+    scrollTrigger:{
+        trigger:"#skillspage",
+        trigger:".skills-content",
+        scroller:"body",
+        // markers:true,
+        start:"680",
+        end:"1250",
+        scrub:2,
+    }
+})
 
 
 const scroll = new LocomotiveScroll({
